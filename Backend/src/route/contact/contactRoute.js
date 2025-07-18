0a11cf106ab0f365e43211aa91a139834b804b70
Sushant Dhakal <sushantdhakal18@gmail.com>
2025-07-18 17:22:10 +0545

    updated

--- Backend/src/route/contact/contactRoute.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { contactController } from "../../controller/index.js";
- 
-const router = express.Router();
-router.post("/", contactController.create);
-router.get("/", contactController.getAll);
- 
-export { router as contactRouter };
- 
--- Backend/src/route/order/orderRoute.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { orderController } from "../../controller/index.js";
- 
-const router = express.Router();
-router.get("/", orderController.getAll);
-router.post("/", orderController.create);
- 
-export { router as orderRouter };
- 
