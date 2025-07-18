c8ea654225c2c682a51218aa1482884b5a85f9b3
Sushant Dhakal <sushantdhakal18@gmail.com>
2025-07-18 15:22:53 +0545

    formated

--- New/Frontend/src/private/SofaSetsPage.jsx
+++
@@ -1,7 +0,0 @@
-import ProductPage from "../components/ProductPage";
-
-const SofaSetsPage = () => {
-  return <ProductPage category="sofa-set" pageTitle="Sofa Sets" />;
-};
-
-export default SofaSetsPage;
