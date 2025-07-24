262487f0251c492d626b88102f7ac3f22988173b
Sushant Dhakal <sushantdhakal18@gmail.com>
2025-07-24 23:32:52 +0545

    improved spacing in main

--- New/Frontend/src/main.jsx
+++ New/Frontend/src/main.jsx
@@ -2,6 +2,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom'; // ✅ Import this
+
 import App from './App.jsx';
 import './index.css';
 
